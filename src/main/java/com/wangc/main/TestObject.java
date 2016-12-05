68b265d8d1f644867e5deaf3e111cd8be1797baa
wangc-ac <wangc-ac@glodon.com>
2016-12-05 14:22:21 +0800

    create project

---
+++ src/main/java/com/wangc/main/TestObject.java
@@ -0,0 +1,32 @@
+package com.wangc.main;
+
+import com.wangc.model.Person;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by wangc-ac on 2016/11/28.
+ * test Object equals
+ * no
+ */
+public class TestObject {
+    public static void main(String[] args){
+        Person p1 = new Person();
+        Person p2 = new Person();
+
+        System.out.println(p1.equals(p2));
+
+        p1.setName("a");
+        p2.setName("b");
+        List<Person> ls = new ArrayList<Person>();
+        ls.add(p1);
+        ls.add(p2);
+        Collections.sort(ls);
+        for (int i=0;i<ls.size();i++){
+            System.out.println(ls.get(i));
+        }
+    }
+
+}
---
+++ src/main/java/com/wangc/model/Person.java
@@ -0,0 +1,59 @@
+package com.wangc.model;
+
+import java.util.Comparator;
+
+/**
+ * Created by wangc-ac on 2016/11/28.
+ */
+public class Person implements Comparable<Person>{
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        return name != null ? name.equals(person.name) : person.name == null;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.name.compareTo(o.getName());
+    }
+}
